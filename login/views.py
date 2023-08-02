0eeab63c608c07d8ca3ed1795daa04e62399130e
tyronejose <alt.tyronejose@gmail.com>
2023-08-02 17:27:07 -0400

    Add file_ views and urls

--- login/views.py
+++ login/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    """
+    Vista de inicio de la página
+    Retorna hacia la home.html
+    """
+    return render(request, 'login/home.html')
+
+
+def products(request):
+    return render(request, 'login/products.html')
