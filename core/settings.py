ee68b79e899f5abf08b7cb566f2ffa08496650da
tyronejose <alt.tyronejose@gmail.com>
2023-08-03 14:40:40 -0400

    Add file_ varibles de entorno

--- core/settings.py
+++ core/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# VARIABLES DE ENTORNO: REDIRECCION DE LOGIN Y LOGOUT
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
